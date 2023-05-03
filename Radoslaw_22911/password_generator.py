8a5b7edfda289c895ad1d5fb9f4c6dbb472cad4d
radekC <radek.curylo60@gmail.com>
2023-05-03 19:04:30 +0200

    Dodano opis działania programu password_generator.py

--- Radoslaw_22911/password_generator.py
+++ Radoslaw_22911/password_generator.py
@@ -1,6 +1,11 @@
 import random
 import os
 
+'''
+Program generujący losowe hasła.
+Użytkownik wprowadza ilość i długość haseł do wygenerowania. Zwracana jest podana liczba ciągów znaków o długości podanej przez użytkownika zawierająca litery losowej wielkości, znaki specjalne oraz cyfry.
+'''
+
 os.system('cls')
 
 chars = 'abcdefghijklmopqrstuvxyzABCDEFGHIJKLMOPQRSTUVXYZ!@#$%^&*()1234567890'
