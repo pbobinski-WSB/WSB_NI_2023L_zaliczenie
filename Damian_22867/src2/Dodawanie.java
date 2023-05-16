0ed363e1ebeb88a001d58f5fcd6b040fc391b272
WSBDamian <wwx15457@student.wsb.warszawa.pl>
2023-05-16 23:37:49 +0200

    Dodanie katalogu Damian_22767 oraz dodanie w nim aplikacji

---
+++ Damian_22867/src2/Dodawanie.java
@@ -0,0 +1,15 @@
+/**
+ * Klasa reprezentująca operację dodawania dwóch liczb.
+ */
+public class Dodawanie {
+    /**
+     * Metoda wykonująca dodawanie dwóch liczb.
+     *
+     * @param a Pierwsza liczba do dodania.
+     * @param b Druga liczba do dodania.
+     * @return Suma dwóch liczb.
+     */
+    public double dodaj(double a, double b) {
+        return a + b;
+    }
+}
---
+++ Damian_22867/test/Uruchomienie.java
@@ -0,0 +1,13 @@
+    /**
+ * Klasa zawierająca metodę main do uruchomienia aplikacji.
+ */
+public class Uruchomienie {
+    /**
+     * Metoda główna programu, która uruchamia interfejs użytkownika.
+     *
+     * @param args argumenty wiersza poleceń (nie są wykorzystywane w tej aplikacji)
+     */
+    public static void main(String[] args) {
+        UI.main();
+    }
+}
