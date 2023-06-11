0066a684f6684d21a8308e6035e10265be6b08b4
Paweł Szymczak <p.szymczak@vp.pl>
2023-06-11 12:44:38 +0200

    Skrypt Dragon.bat

---
+++ Pawel_22489/Dragon.bat
@@ -0,0 +1,49 @@
+@echo off
+color 0d
+title Dragon
+
+
+
+echo                                                                                       ..:::::..     
+echo                                                                                 .:^!7J5PPPP57:.     
+echo                                                                           .:~!?JYYYYYYYY5YY7        
+echo                                                                         ^JY5YYYJJYJJJYYYYYY!        
+echo                                                                        ^P5YJ???JYY55Y55555YJ:       
+echo                                                                       ~5?77?JY55YY5PP555PPPPY^      
+echo                                                                      757!!JPP5555PPPPGPPGGGGGP7.    
+echo                                                                    :YPJJ55Y7!777?JJJJJJ??JYYY5PP?:  
+echo                                  ^7?7.                       .. ..!GGPP5J7!!77??????JJ?77777!7JJ7:: 
+echo                                   .~JJ!^^:                   ..^Y5GGGP5YJ?7~~~!!!777???77777??7:    
+echo                                ... :!P5JY7:.                  :JGGJYYYYYYYJJ?????77???77777??.     
+echo                                :~!7?JJP5Y5?:.                    7B5~^^~!!7?77?JJYY?7!7!!777?^      
+echo                                :::....:~YGPY~.                    ?B7:^~~7??7~~!7JJYJJJ?777?J:      
+echo                                          !PGPY!                   :GY:^^~!!!!!77???!!77JYYJ?J^      
+echo                                           :YP5GY~                  ?B!^^:::::^~!77!~~^^^~7?JJ7.     
+echo                                            .755GB7                 JBY^^::.:.::^^~!~~~~^:::^!??:    
+echo                                             .BPJPBY:             .~PGG~:::...:^^~!!~^:::......:^.   
+echo                                              !G77PBBJ7!^^!?JJJJJJ5PGGG5^::..::::^~~:.               
+echo                                               YJ^?PB#BBPBBGGGGP5YYYYJJ5P!:.....::.                  
+echo                        ..                    .5P?JJ5BGPPGB#P?7~~^^^!!!~?5!...::.                    
+echo             ..::::^^~!7JYJJ~.                Y#PJ?5PPYPGGGBG5~~~^::^^^^^!57:::                      
+echo          ^7?JJJ?777J5PGPPPGPY?!^.         .^JBBPJ7?JJ5PGGGGG5~.....::^^^~~YJ^.                      
+echo       .!?J7!~!!!J5Y?7!!7??Y5???JJ7~:.   :J5GBPGGP5JJJJJYPGGGGGY7~.        .~~                       
+echo     :!?7!!!!!7!?GJ!~~~!!!^!P???J??JJYYJqJPP5YYY5PYYP5J?7?YPBB#ss#ff^                                
+echo   ^?Y7~~!!!????PY7!~~~~~~~!P?77???!!7?YPBGY?!~^~!^.:7JJJ??JBBBBBBGGG57:                             
+echo  7PY?7777??J??PY!~^^~~^^~~^?Y?????!!!!77J5?!~^:^^.   .:^7YP5PPPPPGGBBGP7:                           
+echo :?~^^^~!7?JJJP5~:..::::^^^::Y?77?7!!7!!!!7Y?^:^~.     7PGBBBBGPPG5?5BBGBG7.                         
+echo .       .^?JJP!:....::::::::7Y^^^~!7??7~!!~?Y~~:     !BPPPPPGPPP!. .~YBBGB57~:                      
+echo           .^YY~:...:::::^^^~~5!::~~^^^:::::^?Y^      ~Y555555P5PY^    ^YBPPGG7.                     
+echo             ~!:..      ...:^^!J:::.          ^^        ::::..::75P?!.   ~PBGG?.                     
+echo                              .7!.                               .?GGY?JJ^.?BGB5.                    
+echo                                :.                                 ^7JY5J:  ^PBGY.                   
+echo                                                                       .     .5BG?                   
+echo                                                                              .YBG!                  
+echo                                                                                YBY.                 
+echo                                                                                .?PJ:                
+echo                                                                                  :7Y7:              
+echo                                                                                    :7J7:            
+echo                                                                                      .^!!^.         
+echo                                                                                         .~!^.       
+echo                                                                                            :~.      
+                                                                                                    
+pause >nul																									
