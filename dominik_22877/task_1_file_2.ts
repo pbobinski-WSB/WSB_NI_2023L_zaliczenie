a2a8c42e88ed31cfdc94f86c4fc77edfeb4838c5
MajkelBuszy <swierzewski.biz@gmail.com>
2023-06-11 14:29:40 +0200

    changes

--- dominik_22877/task_1_file_2.ts
+++ dominik_22877/task_1_file_2.ts
@@ -1,19 +1,4 @@
 console.log('change 2');
 console.log('change 3');
 console.log('change 7');
-<<<<<<< HEAD
-console.log('change 8');
-console.log('change 2');
-console.log('change 3');
-console.log('change 7');
-console.log('change 8');
-console.log('change 2');
-console.log('change 3');
-console.log('change 7');
-console.log('change 8');
-console.log('change 2');
-console.log('change 3');
-console.log('change 7');
-=======
->>>>>>> parent of 834a801 (another change to file)
 console.log('change 8');
