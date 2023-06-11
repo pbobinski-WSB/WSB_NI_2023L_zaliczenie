a9d84eecbd5cb8d33064ae227e495c40df18942d
MajkelBuszy <swierzewski.biz@gmail.com>
2023-06-11 14:28:21 +0200

    reverted

--- dominik_22877/task_1_file_2.ts
+++ dominik_22877/task_1_file_2.ts
@@ -1,6 +1,7 @@
 console.log('change 2');
 console.log('change 3');
 console.log('change 7');
+<<<<<<< HEAD
 console.log('change 8');
 console.log('change 2');
 console.log('change 3');
@@ -13,4 +14,6 @@ console.log('change 8');
 console.log('change 2');
 console.log('change 3');
 console.log('change 7');
+=======
+>>>>>>> parent of 834a801 (another change to file)
 console.log('change 8');
