f65acb7e431c4dd0e34426e94051e56b0fc7e801
MajkelBuszy <swierzewski.biz@gmail.com>
2023-06-11 14:29:55 +0200

    change before revert

--- dominik_22877/task_1_file_2.ts
+++ dominik_22877/task_1_file_2.ts
@@ -2,3 +2,7 @@ console.log('change 2');
 console.log('change 3');
 console.log('change 7');
 console.log('change 8');
+console.log('change 2');
+console.log('change 3');
+console.log('change 7');
+console.log('change 8');
