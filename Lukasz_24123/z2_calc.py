3cdb257ea910064d32c747b602528b7f21fbc8a3
Lukasz <bleyosss@wp.pl>
2023-06-12 22:02:06 +0200

    cube operation was added

--- Lukasz_24123/z2_calc.py
+++ Lukasz_24123/z2_calc.py
@@ -1,5 +1,7 @@
 def calculate_square(num):
     return num * num
 
+def calculate_cube(num):
+    return num*num*num
 result = int(input("Podaj liczbe calkowita: \n"))
-print("Kwadrat liczby: ",result, "wynosi: ", calculate_square(result))
+print("Kwadrat liczby: ",result, "wynosi: ", calculate_square(result)," a sześcian: ",calculate_cube(result))
