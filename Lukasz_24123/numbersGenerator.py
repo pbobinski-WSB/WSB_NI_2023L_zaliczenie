fb36c2d44b890ba048ebf733691e9be1a844959f
Lukasz <bleyosss@wp.pl>
2023-06-12 18:25:04 +0200

    numbers summation was added

--- Lukasz_24123/numbersGenerator.py
+++ Lukasz_24123/numbersGenerator.py
@@ -4,3 +4,8 @@ numbers = [random.randint(1, 1000) for i in range(9)]
 
 print("Wygenerowane liczby to:")
 print(numbers)
+
+sum_of_numbers = sum(sorted_numbers)
+
+print("Suma liczb to:")
+print(sum_of_numbers)
