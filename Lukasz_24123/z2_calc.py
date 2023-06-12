f067b118ee9556323a446cf0b93e28fe0e8a6f69
Lukasz <bleyosss@wp.pl>
2023-06-12 22:03:27 +0200

    greeting was added

--- Lukasz_24123/z2_calc.py
+++ Lukasz_24123/z2_calc.py
@@ -4,4 +4,5 @@ def calculate_square(num):
 def calculate_cube(num):
     return num*num*num
 result = int(input("Podaj liczbe calkowita: \n"))
+print("Witaj w kalkulatorze!!!")
 print("Kwadrat liczby: ",result, "wynosi: ", calculate_square(result)," a sześcian: ",calculate_cube(result))
