c4d56b585adbb2ecdb5b4d25c7627bd8fad2647f
Miłosz <milosz.urbanski02@gmail.com>
2023-06-19 21:29:24 +0200

    Revert "Ta zmiana wprowadza funkcje oblicz_bmi() i interpretuj_bmi(), które są odpowiedzialne za obliczenie wskaźnika masy ciała (BMI) oraz interpretację wyniku. Funkcje te ułatwiają czytelność kodu i umożliwiają ponowne wykorzystanie kodu w przyszłości."
    This reverts commit 5b96f69140143bd2a4de6c2daa962e77bafaa20c.

--- MiłoszUrbański_22939/plik_1.py
+++ MiłoszUrbański_22939/plik_1.py
@@ -1,23 +1,16 @@
-def oblicz_bmi(waga, wzrost):
-    bmi = waga / (wzrost ** 2)
-    return bmi
-
-def interpretuj_bmi(bmi):
-    if bmi < 18.5:
-        return "Masz niedowagę."
-    elif bmi >= 18.5 and bmi < 25:
-        return "Twoja waga jest prawidłowa."
-    else:
-        return "Masz nadwagę."
-
 try:
     waga = float(input("Podaj swoją wagę (w kg): "))
     wzrost = float(input("Podaj swój wzrost (w metrach): "))
 
-    wynik_bmi = oblicz_bmi(waga, wzrost)
-    print("Twoje BMI wynosi:", wynik_bmi)
-    interpretacja = interpretuj_bmi(wynik_bmi)
-    print(interpretacja)
+    bmi = waga / (wzrost ** 2)
+    print("Twoje BMI wynosi:", bmi)
+
+    if bmi < 18.5:
+        print("Masz niedowagę.")
+    elif bmi >= 18.5 and bmi < 25:
+        print("Twoja waga jest prawidłowa.")
+    else:
+        print("Masz nadwagę.")
 
 except ValueError:
     print("Wprowadzono nieprawidłowe dane. Podaj wartości liczbowe.")
