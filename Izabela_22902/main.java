3f696a6a7c986fca740a6092595f4991cd6b1d6a
unknown <wwx13685@student.wsb.warszawa.pl>
2023-06-19 22:56:52 +0200

    Revert "Dodanie pętli wypisującej liczby"
    This reverts commit 9762f0ecabeff1feb05c372d2d188531e204a0e2.

--- Izabela_22902/main.java
+++ Izabela_22902/main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Witaj świecie!");
-
-        //Wypisanie liczb od 1 do 5
-        for (int i = 1; i <= 5; i++) {
-        System.out.println("i = " + i);
-        
-        }
     }
 }
