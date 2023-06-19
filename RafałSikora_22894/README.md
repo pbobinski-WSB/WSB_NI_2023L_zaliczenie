133f04a5fe647358bff13c3cdebcc050b010dc32
RajfoR <rafalsikora146@gmail.com>
2023-06-19 19:09:39 +0200

    Dodano dwa pliki

---
+++ RafałSikora_22894/README.md
@@ -0,0 +1,3 @@
+Imię: Rafał
+numer albumu: 22894
+numer grupy: c11
---
+++ RafałSikora_22894/temperature.py
@@ -0,0 +1,4 @@
+celsjusz = float(input("Podaj temperaturę w stopniach Celsjusza: "))
+fahrenheity = (celsjusz * 9/5) + 32
+
+print("Temperatura w stopniach Fahrenheita wynosi:", fahrenheity)
