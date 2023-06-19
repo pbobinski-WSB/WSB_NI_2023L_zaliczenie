af673648d72ae58490b6068c4ded21b3d435a1d4
RajfoR <rafalsikora146@gmail.com>
2023-06-19 19:11:20 +0200

    Rozbudowa temperature.py

--- RafałSikora_22894/temperature.py
+++ RafałSikora_22894/temperature.py
@@ -1,4 +1,10 @@
-celsjusz = float(input("Podaj temperaturę w stopniach Celsjusza: "))
+while True:
+    try:
+        celsjusz = float(input("Podaj temperaturę w stopniach Celsjusza: "))
+        break
+    except ValueError:
+        print("Błąd! Wprowadź poprawną liczbę.")
+
 fahrenheity = (celsjusz * 9/5) + 32
 
 print("Temperatura w stopniach Fahrenheita wynosi:", fahrenheity)
