96cc539f89acf659ef9595684e008f53a7b5db10
Miłosz <milosz.urbanski02@gmail.com>
2023-06-19 21:28:05 +0200

    Ta zmiana również wprowadza funkcje celsius_to_fahrenheit() i fahrenheit_to_celsius(), które są odpowiedzialne za konwersję temperatury. Funkcje te zwiększają czytelność kodu i umożliwiają ponowne wykorzystanie kodu w przyszłości.

--- MiłoszUrbański_22939/plik_2.py
+++ MiłoszUrbański_22939/plik_2.py
@@ -1,12 +1,23 @@
-celsius = float(input("Podaj temperaturę w stopniach Celsiusza: "))
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
 
-fahrenheit = (celsius * 9/5) + 32
-print("Temperatura w stopniach Fahrenheit wynosi:", fahrenheit)
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
 
-# Dodatkowa funkcjonalność: Sprawdzenie, czy jest to temperatura powyżej lub poniżej punktu zamarzania wody
-if fahrenheit <= 32:
-    print("To jest temperatura poniżej punktu zamarzania wody.")
-elif fahrenheit >= 212:
-    print("To jest temperatura powyżej punktu wrzenia wody.")
-else:
-    print("To jest temperatura w normalnym zakresie.")
+try:
+    temperatura = float(input("Podaj temperaturę: "))
+    jednostka = input("Wybierz jednostkę (C - stopnie Celsiusza, F - stopnie Fahrenheita): ")
+
+    if jednostka.upper() == "C":
+        fahrenheit = celsius_to_fahrenheit(temperatura)
+        print("Temperatura w stopniach Fahrenheit wynosi:", fahrenheit)
+    elif jednostka.upper() == "F":
+        celsius = fahrenheit_to_celsius(temperatura)
+        print("Temperatura w stopniach Celsiusza wynosi:", celsius)
+    else:
+        print("Nieprawidłowy wybór jednostki.")
+
+except ValueError:
+    print("Wprowadzono nieprawidłową temperaturę. Podaj wartość liczbową.")
