0bca38cbaaa1ea7ccd6fb811658721ce0bbfcbb0
DawidRD7 <137209422+DawidRD7@users.noreply.github.com>
2023-06-21 20:33:07 +0200

    Dodanie komentarzy

--- Dawid_22852/ShoppingCart.java
+++ Dawid_22852/ShoppingCart.java
@@ -10,14 +10,17 @@ public class ShoppingCart {
         this.products = new ArrayList<>();
     }
 
+    // Metoda dodająca produkt do koszyka
     public void addProduct(Product product) {
         products.add(product);
     }
 
+    // Metoda usuwająca produkt z koszyka
     public void removeProduct(Product product) {
         products.remove(product);
     }
 
+    // Metoda zwracająca listę produktów w koszyku
     public List<Product> getProducts() {
         return products;
     }
