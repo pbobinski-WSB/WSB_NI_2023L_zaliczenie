a34841872ea783fd92f72ddc6258a22a98d4a44c
DawidRD7 <137209422+DawidRD7@users.noreply.github.com>
2023-06-21 20:31:35 +0200

    Dodanie pliku ShoppingCart

---
+++ Dawid_22852/ShoppingCart.java
@@ -0,0 +1,24 @@
+package com.myapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<Product> products;
+
+    public ShoppingCart() {
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
