4ed5f493e2e9aec8373d16b87fd47d2128269235
DawidRD7 <137209422+DawidRD7@users.noreply.github.com>
2023-06-21 18:50:47 +0200

    Update Main.java
    Zmiana cen produktów

--- Dawid_22852/Main.java
+++ Dawid_22852/Main.java
@@ -6,9 +6,9 @@ import com.myapp.service.ShopService;
 
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product("Laptop", 2500.0);
-        Product product2 = new Product("Smartphone", 1200.0);
-        Product product3 = new Product("Headphones", 150.0);
+        Product product1 = new Product("Laptop", 3000.0);
+        Product product2 = new Product("Smartphone", 1500.0);
+        Product product3 = new Product("Headphones", 250.0);
 
         ShoppingCart cart = new ShoppingCart();
         ShopService shopService = new ShopService();
