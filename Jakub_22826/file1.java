6e06a24a0492d4672c8f393fed826d2353819db4
Kuba2408 <jakub.wieczorek.2408@gmail.com>
2023-06-22 01:24:51 +0200

    Auto stash before revert of "test file 1 and file 2"

--- Jakub_22826/file1.java
+++ Jakub_22826/file1.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("file 1");
         System.out.println("Update");
-        System.out.println("Test1");
     }
 }
--- Jakub_22826/file2.java
+++ Jakub_22826/file2.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("file 2");
         System.out.println("Update2");
-        System.out.println("Test2");
     }
 }
