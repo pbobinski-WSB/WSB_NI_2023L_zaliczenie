5bc4b5f2a21c20fede3d26cb48965abb8be347b3
DawidRD7 <137209422+DawidRD7@users.noreply.github.com>
2023-06-21 21:07:48 +0200

    Dokumentacja javadoc

--- Dawid_22852/ShoppingCart.java
+++ Dawid_22852/ShoppingCart.java
@@ -3,24 +3,42 @@ package com.myapp.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Klasa ShoppingCart reprezentuje koszyk zakupowy, który przechowuje listę produktów.
+ */
 public class ShoppingCart {
     private List<Product> products;
 
+    /**
+     * Konstruktor tworzy nowy pusty koszyk zakupowy.
+     */
     public ShoppingCart() {
         this.products = new ArrayList<>();
     }
 
-    // Metoda dodająca produkt do koszyka
+    /**
+     * Dodaje produkt do koszyka zakupowego.
+     *
+     * @param product produkt, który ma zostać dodany do koszyka
+     */
     public void addProduct(Product product) {
         products.add(product);
     }
 
-    // Metoda usuwająca produkt z koszyka
+    /**
+     * Usuwa produkt z koszyka zakupowego.
+     *
+     * @param product produkt, który ma zostać usunięty z koszyka
+     */
     public void removeProduct(Product product) {
         products.remove(product);
     }
 
-    // Metoda zwracająca listę produktów w koszyku
+    /**
+     * Pobiera listę produktów znajdujących się w koszyku zakupowym.
+     *
+     * @return lista produktów w koszyku zakupowym
+     */
     public List<Product> getProducts() {
         return products;
     }
