d41ad2683961f58ed3b96acfcafc2f919f28221d
DawidRD7 <137209422+DawidRD7@users.noreply.github.com>
2023-06-21 21:12:34 +0200

    Dokumentacja javadoc

--- Dawid_22852/ShopServiceTest.java
+++ Dawid_22852/ShopServiceTest.java
@@ -6,12 +6,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Klasa ShopServiceTest zawiera testy jednostkowe dla klasy ShopService.
+ */
 public class ShopServiceTest {
     private ShopService shopService;
     private ShoppingCart cart;
     private Product product1;
     private Product product2;
 
+    /**
+     * Przygotowanie danych przed wykonaniem każdego testu.
+     */
     @BeforeEach
     public void setup() {
         shopService = new ShopService();
@@ -20,6 +26,9 @@ public class ShopServiceTest {
         product2 = new Product("Product 2", 20.0);
     }
 
+    /**
+     * Testuje dodawanie produktu do koszyka.
+     */
     @Test
     public void testAddToCart() {
         shopService.addToCart(cart, product1);
@@ -29,6 +38,9 @@ public class ShopServiceTest {
         Assertions.assertEquals(2, cart.getProducts().size());
     }
 
+    /**
+     * Testuje usuwanie produktu z koszyka.
+     */
     @Test
     public void testRemoveFromCart() {
         shopService.addToCart(cart, product1);
@@ -39,6 +51,9 @@ public class ShopServiceTest {
         Assertions.assertEquals(product2, cart.getProducts().get(0));
     }
 
+    /**
+     * Testuje obliczanie całkowitej ceny zamówienia.
+     */
     @Test
     public void testCalculateTotalPrice() {
         shopService.addToCart(cart, product1);
