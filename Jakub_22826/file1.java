01cfab3893b73d54e0d5c46ecc7ba750fdf2fcfd
Kuba2408 <jakub.wieczorek.2408@gmail.com>
2023-06-22 01:23:23 +0200

    test file 1 and file 2
    file1.java print "Test1"
    file2.java print "Test2"

--- Jakub_22826/file1.java
+++ Jakub_22826/file1.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("file 1");
         System.out.println("Update");
+        System.out.println("Test1");
     }
 }
