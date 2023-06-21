e6d8e4ae71f486a2219f109f356cdb0fa93a84e3
DawidRD7 <137209422+DawidRD7@users.noreply.github.com>
2023-06-21 18:54:46 +0200

    Dodanie komentarzy

--- Dawid_22852/Product.java
+++ Dawid_22852/Product.java
@@ -1,27 +1,34 @@
 package com.myapp.model;
 
+
 public class Product {
     private String name;
     private double price;
 
+
     public Product(String name, double price) {
+        // Konstruktor klasy Product
         this.name = name;
         this.price = price;
     }
 
     public String getName() {
+        // Metoda getName zwraca nazwę produktu
         return name;
     }
 
     public void setName(String name) {
+        // Metoda setName ustawia nową nazwę produktu na podstawie przekazanego argumentu
         this.name = name;
     }
 
     public double getPrice() {
+        // Metoda getPrice zwraca cenę produktu
         return price;
     }
 
     public void setPrice(double price) {
+        // Metoda setPrice ustawia nową cenę produktu na podstawie przekazanego argumentu
         this.price = price;
     }
 }
