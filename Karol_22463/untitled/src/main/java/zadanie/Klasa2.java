18fb455a8a9e876aec29ed57cb9b5d02ae023eb7
KarlStazysta <mlynekarol@gmail.com>
2023-06-22 17:59:40 +0200

    dodano zawartosc do klasy Klasa2

--- Karol_22463/untitled/src/main/java/zadanie/Klasa2.java
+++ Karol_22463/untitled/src/main/java/zadanie/Klasa2.java
@@ -1,4 +1,14 @@
 package zadanie;
 
+/**
+ * Ta klasa posiada jedną metodę która zwraca int oraz dwie zmienne, które są wypisywane przez main za pomocą Lombok @ToString
+ */
 public class Klasa2 {
+    int a = 19,b = 21;
+    public void kalk(){
+        System.out.println(a+b);
+    }
+    public int kalk1(){
+        return 39;
+    }
 }
