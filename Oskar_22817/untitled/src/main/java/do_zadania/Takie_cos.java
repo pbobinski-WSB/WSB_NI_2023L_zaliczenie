f9a54daa3f4771f9f495e533e134ee7f64328a61
Czujsi <oskarbanaszczuk@gmail.com>
2023-06-22 20:33:25 +0200

    dodano klasy i uzupełniono metodami

--- Oskar_22817/untitled/src/main/java/do_zadania/Takie_cos.java
+++ Oskar_22817/untitled/src/main/java/do_zadania/Takie_cos.java
@@ -1,5 +1,12 @@
 package do_zadania;
 
+import lombok.ToString;
+
+/**
+ * Ta klasa posiada jedną metodę która zwraca int oraz dwie zmienne,
+ * które są wypisywane przez main'a za pomocą Lombok @ToString
+ */
+@ToString
 public class Takie_cos {
     int a =19,b = 20;
     //żeby to wypisać używam lombok toString
