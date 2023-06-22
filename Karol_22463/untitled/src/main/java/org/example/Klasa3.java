1b559c50a1567249c7df07159d99b70155bc034d
KarlStazysta <mlynekarol@gmail.com>
2023-06-22 18:06:31 +0200

    Revert "dodano zawartosc klasy Klasa3"
    This reverts commit 47b35b3619b3137b5a6b47fc62e5ccebd5333028.

--- Karol_22463/untitled/src/main/java/org/example/Klasa3.java
+++ Karol_22463/untitled/src/main/java/org/example/Klasa3.java
@@ -1,21 +1,4 @@
 package org.example;
 
-import zadanie.Klasa;
-import zadanie.Klasa2;
-
-/**
- * ta klasa importuje klasę Klasa z pakietu - zadanie
- * posiada dwie metody
- * jedna zwraca int
- * druga zwraca string
- */
 public class Klasa3 {
-    Klasa2 klasa2 = new Klasa2();
-    Klasa klasa = new Klasa();
-    public  int kalk2(){
-        return klasa2.kalk1();
-    }
-    public String toPisze(){
-        return  klasa.nazwa();
-    }
 }
