83938b9348d26838147846a5e3030f02eee8f906
PrzemysT <przemyslaw.telutki@o2.pl>
2023-06-22 20:59:28 +0200

    Dodano zawartość do klas testowych

--- Przemyslaw_22492/projekt/src/test/java/org/example/Klasa3Test.java
+++ Przemyslaw_22492/projekt/src/test/java/org/example/Klasa3Test.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Klasa3Test {
+    @Test
+    public void testyy() {
+        Klasa3 klasa3 = new Klasa3();
+        Assertions.assertEquals(klasa3.text(), "Testy");
 
+    }
+    @Test
+    public void testyy1() {
+        Klasa3 klasa3 = new Klasa3();
+        Assertions.assertEquals(klasa3.matma1(), 3);
+
+    }
 }
