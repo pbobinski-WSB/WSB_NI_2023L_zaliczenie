6f7dd3505eefb8e628fbc005f534caed23fe9ce2
Kordek3 <konradbieniak@outlook.com>
2023-06-23 23:39:21 +0200

    Dodanie składu PSG

--- Konrad_22422/Final_20.py
+++ Konrad_22422/Final_20.py
@@ -13,8 +13,27 @@ sklad_bayernu = [
     ["9", "Robert Lewandowski", "Napastnik"]
 ]
 
+sklad_psg = [
+    ["Numer", "Zawodnik", "Pozycja"],
+    ["1", "Keylor Navas", "Bramkarz"],
+    ["4", "Thilo Kehrer", "Obrońca"],
+    ["3", "Presnel Kimpembe", "Obrońca"],
+    ["5", "Marquinhos", "Obrońca"],
+    ["14", "Juan Bernat", "Obrońca"],
+    ["8", "Leandro Paredes", "Pomocnik"],
+    ["6", "Marco Verratti", "Pomocnik"],
+    ["11", "Ángel Di María", "Pomocnik"],
+    ["7", "Kylian Mbappé", "Napastnik"],
+    ["10", "Neymar", "Napastnik"],
+    ["9", "Mauro Icardi", "Napastnik"]
+
 # Wypisanie składu Bayernu Monachium
 for pozycja in sklad_bayernu:
     print("{:<5} {:<20} {:<10}".format(pozycja[0], pozycja[1], pozycja[2]))
 
 print("\n")
+
+# Wypisanie składu PSG
+print("Skład PSG:")
+for pozycja in sklad_psg:
+    print("{:<5} {:<20} {:<10}".format(pozycja[0], pozycja[1], pozycja[2]))
