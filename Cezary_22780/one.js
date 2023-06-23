bd8f1bd2e3b6a509f706d846cd9a58735f01134c
Cezary <cezaryszewczyk2001@o2.pl>
2023-06-23 20:00:42 +0200

    changed files fourth time

--- Cezary_22780/one.js
+++ Cezary_22780/one.js
@@ -1,3 +1,4 @@
 console.log('jeden');
 console.log('jedenn');
 console.log('jedennn');
+console.log('jedennnn');
--- Cezary_22780/two.js
+++ Cezary_22780/two.js
@@ -1,3 +1,4 @@
 console.log('jeden1');
 console.log('jeden11');
 console.log('jeden111');
+console.log('jeden1111');
