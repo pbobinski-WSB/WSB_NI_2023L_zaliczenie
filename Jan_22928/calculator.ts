514fbe4bab706b895d611eedaf63cf6e7c86c9d7
Jan Rudowski <j.rudowski@protonmail.com>
2023-06-23 19:19:45 +0200

    implementacja poprawnej metody do dzielenia

--- Jan_22928/calculator.ts
+++ Jan_22928/calculator.ts
@@ -6,6 +6,15 @@ class Calculator {
   public substract(x: number, y: number): number {
     return x - y;
   }
+
+  public multiply(x: number, y: number): number {
+    return x * y;
+  }
+
+  public divide(x: number, y: number): number {
+    if (y === 0) throw new Error("Division by zero");
+    return x / y;
+  }
 }
 
 export default new Calculator();
