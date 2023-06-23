9b9f1cec9563cca5c828a731e6e2e69059cfdae2
Jan Rudowski <j.rudowski@protonmail.com>
2023-06-23 19:30:25 +0200

    dodanie metody do konwersji mil na km

--- Jan_22928/converter.ts
+++ Jan_22928/converter.ts
@@ -1,11 +1,16 @@
 class Converter {
-  private cmInInch: number = 2.54;
+  private readonly cmInInch: number = 2.54;
+  private readonly kmInMile: number = 1.609344;
   public inchesToCm(inches: number): number {
     return inches * this.cmInInch;
   }
 
   public fahrenheitToCelsius(temp: number): number {
     return (temp - 32) * (5 / 9);
+  }
+
+  public mileToKm(miles: number): number {
+    return miles * this.kmInMile;
   }
 }
 
