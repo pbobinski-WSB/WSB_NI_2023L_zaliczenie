9a0e8bdef4d4cd6deb0617b7c132b85af0922148
Kordek3 <konradbieniak@outlook.com>
2023-06-24 00:05:38 +0200

    Usunięcie o królu strzelców i stadionie finału
    This reverts commit 53206d171aba8aafe46eae60c4287700850c6579.

--- Konrad_22422/niemcy_2006.py
+++ Konrad_22422/niemcy_2006.py
@@ -3,8 +3,6 @@ import pandas as pd
 data = {
     'Rok': [2006],
     'Zwycięzca': ['Włochy']
-    'Król strzelców': ['Miroslav Klose'],
-    'Stadion': ['Olympiastadion w Berlinie']
 }
 
 df = pd.DataFrame(data)
