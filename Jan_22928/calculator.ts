c99f8e6be2661e9deea1a63a6c5fcb26cd10dce5
Jan Rudowski <j.rudowski@protonmail.com>
2023-06-23 19:12:59 +0200

    dodanie metody do odejmowania 2 liczb

--- Jan_22928/calculator.ts
+++ Jan_22928/calculator.ts
@@ -2,6 +2,10 @@ class Calculator {
   public add(x: number, y: number): number {
     return x + y;
   }
+
+  public substract(x: number, y: number): number {
+    return x - y;
+  }
 }
 
 export default new Calculator();
