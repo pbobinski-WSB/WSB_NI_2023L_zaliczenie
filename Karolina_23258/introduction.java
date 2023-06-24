d0aa166d9368e3ea5fad5fa93fd6e07031bac93f
dabrosia <atoyotagt86@gmail.com>
2023-06-24 18:46:13 +0200

    first commit to inroduction.java

--- Karolina_23258/introduction.java
+++ Karolina_23258/introduction.java
@@ -1,5 +1,12 @@
 public class introduction {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Cześć, chyba jeszcze nie znasz mnie\n" +
+                "Jestem pierwszy dzień w tej grze\n" +
+                "Podpowiedziałbyś co, gdzie?\n" +
+                "W przygodzie z rapem\n" +
+                "Cześć, chyba jeszcze nie znasz mnie\n" +
+                "Jestem pierwszy dzień w tej grze\n" +
+                "Może na ten hajs i fame\n" +
+                "Też się załapię");
     }
 }
