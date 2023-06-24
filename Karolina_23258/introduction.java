d2207cf077a0e85c30ab7105c62073b9686845df
dabrosia <atoyotagt86@gmail.com>
2023-06-24 18:46:34 +0200

    second commit to inroduction.java

--- Karolina_23258/introduction.java
+++ Karolina_23258/introduction.java
@@ -1,12 +1,14 @@
 public class introduction {
     public static void main(String[] args) {
-        System.out.println("Cześć, chyba jeszcze nie znasz mnie\n" +
-                "Jestem pierwszy dzień w tej grze\n" +
-                "Podpowiedziałbyś co, gdzie?\n" +
-                "W przygodzie z rapem\n" +
-                "Cześć, chyba jeszcze nie znasz mnie\n" +
-                "Jestem pierwszy dzień w tej grze\n" +
-                "Może na ten hajs i fame\n" +
-                "Też się załapię");
+        String message = createMessage("John");
+        printMessage(message);
+    }
+
+    public static String createMessage(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public static void printMessage(String message) {
+        System.out.println("The message is: " + message);
     }
 }
