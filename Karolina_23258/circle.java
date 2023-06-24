b05b72247d3232fc1afd2624cdf607852dc24a40
dabrosia <atoyotagt86@gmail.com>
2023-06-24 18:42:33 +0200

    first commit to circle.java - added description

--- Karolina_23258/circle.java
+++ Karolina_23258/circle.java
@@ -4,6 +4,9 @@ public class circle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Ten kod pobiera od użytkownika wartość promienia koła, a następnie oblicza i
+        //wyświetla jego obwód i pole na podstawie wprowadzonego promienia.
+
         System.out.println("Podaj promień koła: ");
         double r = scanner.nextDouble();
 
