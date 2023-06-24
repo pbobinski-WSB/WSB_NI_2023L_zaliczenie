01b3a75bf5a66cac50dc2ff03272bd8a80398be1
Your Name <lukaszgejzler1@gmail.com>
2023-06-24 14:01:55 +0200

    Modyfikacje wartości

--- Łukasz_22847/New2.py
+++ Łukasz_22847/New2.py
@@ -1,4 +1,7 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
 num1 = 10
 num2 = 20
-sum = num1 + num2
-print("The sum is:", sum)
+sum = add_numbers(num1, num2)
+print("The sum of", num1, "and", num2, "is:", sum)
