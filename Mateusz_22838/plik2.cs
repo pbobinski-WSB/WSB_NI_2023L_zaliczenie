4ebd0464d7f33eab0ee0590ec3a19533b247ab24
Mateusz <wwx15251@student.wsb.warszawa.pl>
2023-06-24 13:30:03 +0200

    Zmieniono tresc wyswietlania plik2

--- Mateusz_22838/plik2.cs
+++ Mateusz_22838/plik2.cs
@@ -4,7 +4,7 @@ namespace plik2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Czeœc jestem plik2");
         }
     }
 }
