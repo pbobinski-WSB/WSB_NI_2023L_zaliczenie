12927c90d5cfa290395e375ce73c813d2ffab244
karolzdziarstek <wwx15617@student.wsb.warszawa.pl>
2023-06-26 22:30:56 +0200

    Dodanie programu srednia.py
    Dodanie programu srednia.py

---
+++ Karol_23252/srednia.py
@@ -0,0 +1,12 @@
+def oblicz_srednia(lista_liczb):
+    return sum(lista_liczb) / len(lista_liczb)
+
+ilosc_liczb = int(input("Podaj ilość liczb, które chcesz wprowadzić? "))
+liczby = []
+
+for i in range(ilosc_liczb):
+    liczba = float(input(f"Podaj liczbę {i+1}: "))
+    liczby.append(liczba)
+
+srednia = oblicz_srednia(liczby)
+print(f"Średnia arytmetyczna wprowadzonych liczb wynosi: {srednia}")
